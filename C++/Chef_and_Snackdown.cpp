e09b33b071caf1d3167756c18cb5465a92acdfe5
Soumyajit <mondalsoumya02@gmail.com>
2022-10-06 08:48:17 +0530

    Files Updated

---
+++ C++/Chef_and_Snackdown.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    if(n==2010||n==2015||n==2016||n==2016||n==2017||n==2019)
+	    {
+	        cout<<"HOSTED"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"NOT HOSTED"<<endl;
+	    }
+	}
+	return 0;
+}
