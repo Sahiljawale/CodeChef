6731761fc1eeeec935e10e4ac15040a9a59070a1
shradham2612 <mahajanss_1@rknec.edu>
2022-10-22 02:52:03 +0530

    Solution for REMOVECARDS

--- Python/Avoid Fix Point.py
+++
@@ -1,9 +0,0 @@
-for _ in range(int(input())):
-    n = int(input())
-    l = list(map(int, input().split()))
-    k = 0
-    for i in range(len(l)):
-        Value = 1 + i + k
-        if Value == l[i]:
-            k += 1
-    print(k)
