9d91b4359e4cc86ac8730f6fb0c52510cb35152a
Suchit Deshmukh <56268987+suchitd11@users.noreply.github.com>
2022-10-24 12:34:20 +0530

    Delete Palindrome_Flipping.py

--- Palindrome_Flipping.py
+++
@@ -1,6 +0,0 @@
-for _ in range(int(input())):
-    n = int(input())
-    s = input()
-    countZero = s.count('0')
-    countOne = s.count('1')
-    print('NO') if countZero%2!=0 and countOne%2!=0 else print('YES')
